e1a9edfaff000ddb05d99abfac9a3ee425480535
Daniel Erazo <danielmauricioerazoespinoza@gmail.com>
2025-07-31 21:28:07 -0500

    Teapot route added

--- blueprints/teapot_bp.py
+++ blueprints/teapot_bp.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 teapot_bp = Blueprint('teapot_bp', __name__)
 
-@teapot_bp.route('', methods=['GET'])
+@teapot_bp.route('', methods=['POST'])
 def status():
     return "I'm a teapot", 418
